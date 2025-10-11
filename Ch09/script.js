6cf8c4186a3734f6ae82813369bc9c39f0ee0461
7heProtagonist <30697367+7h3Pr074g0n157@users.noreply.github.com>
2025-10-11 11:31:39 +0200

    Refactor moving letters to CSS aproach + first try reset function

--- Ch09/script.js
+++ Ch09/script.js
@@ -1,22 +1,61 @@
 const inputFields = document.querySelectorAll(".input-field");
-// const inputlabels = document.querySelectorAll(".input-label");
 
-function handleClick(e) {
-  // console.log(e.target);
-  const letters = [
-    ...e.target.parentElement.querySelector(".input-label").children
-  ];
-  // console.log(letters);
-let currentIndex = 0;
-  const intervalID = setInterval(()=>{
-    const currentLetter = letters[currentIndex]
-    currentLetter.style.color = 'red';
-    currentLetter.style.top = '-1rem';
+function getActiveLable(e) {
+  const target = e.target;
+  const activeLable = target.parentElement.querySelector(".input-label");
+  const children = [...activeLable.children];
+  return children;
+}
+
+function handleFocus(e) {
+  e.stopPropagation();
+  console.log(e.target.tagName);
+  const letters = getActiveLable(e);
+
+  let currentIndex = 0;
+  const intervalID = setInterval(() => {
+    const currentLetter = letters[currentIndex];
+    classesOfCurrentLetter = [...currentLetter.classList];
+    // console.log(classesOfCurrentLetter);
+
+    if (!classesOfCurrentLetter.includes("translate")) {
+      currentLetter.classList.add("translate");
+      currentLetter.classList.remove("initial");
+    } else {
+      currentLetter.classList.add("initial");
+      currentLetter.classList.remove("translate");
+    }
+
     currentIndex++;
-    if(currentIndex>= letters.length) clearInterval(intervalID);
-}, 2000)
+    if (currentIndex >= letters.length) clearInterval(intervalID);
+  }, 100);
+}
+
+function handleReset(e) {
+  if (e.target.tagName !== "INPUT") {
+    console.log(e.target.tagName);
+    e.stopImmediatePropagation();
+    const letters = getActiveLable(e);
+    let currentIndex = 0;
+    const intervalID = setInterval(() => {
+      const currentLetter = letters[currentIndex];
+      classesOfCurrentLetter = [...currentLetter.classList];
+
+      if (classesOfCurrentLetter.includes("translate")) {
+        currentLetter.classList.add("initial");
+        currentLetter.classList.remove("translate");
+      }
+
+      currentIndex++;
+      if (currentIndex >= letters.length) clearInterval(intervalID);
+    }, 100);
+  } else {
+    return;
+  }
 }
 
 for (const inputField of inputFields) {
-  inputField.addEventListener("click", handleClick);
+  inputField.addEventListener("focus", handleFocus);
+  inputField.addEventListener("blur", handleFocus);
 }
+window.addEventListener("click", handleReset);
