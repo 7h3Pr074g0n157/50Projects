097f1a49cfdbf740cd194e016f7e2173c73cfe94
7heProtagonist <30697367+7h3Pr074g0n157@users.noreply.github.com>
2025-11-27 18:14:34 +0100

    Refactor logic updating big bottle

--- Ch17/script.js
+++ Ch17/script.js
@@ -1,10 +1,15 @@
 const bigBottle = document.getElementById("2l-bottle");
+const remainingLiter = document.getElementById("remaining-liter");
+const percentDrank = document.getElementById("percent-drank");
 const littleBottles = document.querySelectorAll(
   ".little-bottles-container > div"
 );
-let remainingLiter = 2,
-  mlDrank = 0,
-  percentDrank = 0;
+const bottleState = {
+  bottlesDrank: 0,
+  remainingLiter: 2,
+  mlDrank: 0,
+  percentDrank: 0
+};
 
 littleBottles.forEach((bottle) => {
   bottle.addEventListener("click", clickBottlesHandler);
@@ -14,35 +19,46 @@ function clickBottlesHandler(ev) {
   ev.preventDefault();
   const clickedBottle = ev.target;
 
+  // console.log(clickedBottle);
   if ([...clickedBottle.classList].includes("fullLittleBottle")) {
     clickedBottle.classList.remove("fullLittleBottle");
-    handleRemainingLiter(false);
-    changeLinearGradient(false);
+    calcDrank(false);
   } else {
     clickedBottle.classList.add("fullLittleBottle");
-    handleRemainingLiter(true);
-    changeLinearGradient(true);
+    calcDrank(true);
   }
 
-  bigBottle.style.color = percentDrank > 50 ? "white" : "blue";
+  bigBottle.style.color = bottleState.percentDrank > 50 ? "white" : "blue";
 }
 
-function changeLinearGradient(changeMLdrank) {
-  mlDrank = changeMLdrank ? mlDrank + 0.25 : mlDrank - 0.25;
-  percentDrank = (mlDrank / 2) * 100;
+function calcDrank(drank) {
+  if (drank) {
+    bottleState.mlDrank += 0.25;
+    if (bottleState.remainingLiter > 0) bottleState.remainingLiter -= 0.25;
+    updateBigBottle();
+  } else {
+    bottleState.mlDrank -= 0.25;
+    if (bottleState.remainingLiter < 2) bottleState.remainingLiter += 0.25;
+    updateBigBottle();
+  }
+  // const bottleList = [...littleBottles];
+  // bottleList.forEach((bottle, i) => {
+  //   if (bottle.id === clickedBottle.id) {
+  //     bottleState.bottlesDrank = i + 1 - bottleState.bottlesDrank;
+  //     console.log("loop", i, bottleState.bottlesDrank);
+  //   }
+  // });
+}
+
+function updateBigBottle() {
+  bottleState.percentDrank = (bottleState.mlDrank / 2) * 100;
 
   bigBottle.style.background = `linear-gradient(to top, 
       #58a7f2 0%, 
-      #58a7f2 ${percentDrank}%, 
-      #fff ${percentDrank}%, 
+      #58a7f2 ${bottleState.percentDrank}%, 
+      #fff ${bottleState.percentDrank}%, 
       #fff 100%`;
-}
 
-function handleRemainingLiter(changeRemainingLiter) {
-  if (changeRemainingLiter) {
-    if (remainingLiter > 0) remainingLiter -= 0.25;
-  } else {
-    if (remainingLiter < 2) remainingLiter += 0.25;
-  }
-  bigBottle.textContent = remainingLiter + "L\nremaining";
+  percentDrank.textContent = bottleState.percentDrank + " %";
+  remainingLiter.textContent = bottleState.remainingLiter + "L\nremaining";
 }
